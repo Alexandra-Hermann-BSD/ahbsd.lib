6c837da2f97fd43b602a6dedbb3217253cef979c
Heinrich Alexandra Hermann <a.hermann@hermann-bsd.de>
2021-02-24 01:37:04 +0100

    Removed Test and added Test_xUnit

---
+++ Test_xUnit/TestClass.cs
@@ -0,0 +1,117 @@
+﻿//
+//  Copyright 2021  Alexandra Hermann – Beratung, Software, Design
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+using System;
+using ahbsd.lib;
+using ahbsd.lib.ApiKey;
+
+namespace Test_xUnit
+{
+    /// <summary>
+    /// A class to demonstrate <see cref="ChangeEventArgs{T}"/> and <see cref="ChangeEventHandler{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">Type of <see cref="Variable"/>.</typeparam>
+    /// <typeparam name="A">Type of API-Key.</typeparam>
+    public class TestClass<T, A> : ApiKeyHolder<A>, ITestClass<T, A>
+    {
+        /// <summary>
+        /// The variable to change.
+        /// </summary>
+        private T _variable;
+
+        /// <summary>
+        /// Happenes when <see cref="Variable"/> has changed.
+        /// </summary>
+        public event ChangeEventHandler<T> OnChange;
+
+        /// <summary>
+        /// Gets or sets a variable.
+        /// </summary>
+        public T Variable
+        {
+            get => _variable;
+            set
+            {
+                ChangeEventArgs<T> cea;
+
+                if (value != null && !value.Equals(_variable))
+                {
+                    cea = new ChangeEventArgs<T>(_variable, value);
+
+                    _variable = value;
+
+                    OnChange?.Invoke(this, cea);
+                }
+                else if (value == null && _variable != null)
+                {
+                    cea = new ChangeEventArgs<T>(_variable, value);
+
+                    _variable = value;
+
+                    OnChange?.Invoke(this, cea);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Constructor without parameters.
+        /// </summary>
+        public TestClass()
+            : base()
+        {
+            _variable = default;
+            OnApiKeyAdded += TestClass_OnApiKeyAdded;
+        }
+
+        /// <summary>
+        /// Constructor with a given variable.
+        /// </summary>
+        /// <param name="v">The given variable.</param>
+        public TestClass(T v)
+            : base()
+        {
+            _variable = v;
+            OnApiKeyAdded += TestClass_OnApiKeyAdded;
+        }
+
+        /// <summary>
+        /// Constructor with a given API-Key.
+        /// </summary>
+        /// <param name="apiKey">The given API-Key.</param>
+        public TestClass(A apiKey)
+            : base(apiKey)
+        {
+            _variable = default;
+            OnApiKeyAdded += TestClass_OnApiKeyAdded;
+        }
+
+        /// <summary>
+        /// Constructor with a given value and a given API-Key.
+        /// </summary>
+        /// <param name="v">The given variable.</param>
+        /// <param name="apiKey">The given API-Key.</param>
+        public TestClass(T v, A apiKey)
+            : base(apiKey)
+        {
+            _variable = v;
+            OnApiKeyAdded += TestClass_OnApiKeyAdded;
+        }
+
+
+        private void TestClass_OnApiKeyAdded(object sender, ApiKeyEventArgs<A> e)
+        {
+            Console.WriteLine("An API-Key ('{0}') was added at Index {1}", e.Value, e.Index);
+        }
+    }
+}
---
+++ Test_xUnit/UnitTest1.cs
@@ -0,0 +1,129 @@
+using System;
+using Xunit;
+using ahbsd.lib;
+using ahbsd.lib.Exceptions;
+using System.Collections.Generic;
+using ahbsd.lib.Tools;
+
+namespace Test_xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestChange()
+        {
+            ITestClass<string, object> t1 = new TestClass<string, object>("Hello", null);
+            ITestClass<object, string> t2, t3;
+            IGenericException<ITestClass<object, string>> exT3;
+            double d;
+            t2 = new TestClass<object, string>("Hello", "A100002");
+            t3 = new TestClass<object, string>(0xAFFE, "0xAFFE");
+            t1.OnChange += T1_OnChange;
+            t2.OnChange += T2_OnChange;
+            t3.OnChange += T3_OnChange;
+
+            t1.Variable = "World";
+            t2.Variable = 15;
+            t1.Variable = string.Empty;
+            t1.Variable = null;
+            t2.Variable = null;
+            t3.Variable = new NotFiniteNumberException("Test", 815.9);
+            t2.Variable = 0b1011101;
+            t2.Variable = 25.9f;
+            t2.Variable = 25.9;
+            t3.Variable = "0xAFFE";
+
+            try
+            {
+                d = (double)t3.Variable;
+                d *= (double)t2.Variable;
+            }
+            catch (Exception ex)
+            {
+                exT3 = new Exception<ITestClass<object, string>>(ex.Message, t3);
+            }
+        }
+
+        [Fact]
+        public void TestPrime()
+        {
+            IList<ulong> numbers = new List<ulong>();
+            bool isPrime;
+            TimeSpan time;
+            uint amount;
+            string isPrimeString;
+
+            for (ulong i = 0; i < 20; i++)
+            {
+                numbers.Add(i);
+            }
+
+            for (ulong i = 1990; i < 2170; i++)
+            {
+                numbers.Add(i);
+            }
+
+            numbers.Add(ulong.MaxValue);
+
+            foreach (var nr in numbers)
+            {
+                isPrime = PrimeCheck.Check(nr);
+                time = PrimeCheck.Time;
+                amount = PrimeCheck.Amount;
+
+                if (isPrime)
+                {
+                    isPrimeString = "is a prime number";
+                }
+                else
+                {
+                    isPrimeString = "is not a prime number";
+                }
+
+                Console.WriteLine(
+                    "{0} {1}. It took {2} times in {3} m, {4} s, {5} ms to check.",
+                    nr,
+                    isPrimeString,
+                    amount,
+                    time.Minutes,
+                    time.Seconds,
+                    time.Milliseconds);
+            }
+
+        }
+
+        private void T3_OnChange(object sender, ChangeEventArgs<object> e)
+        {
+            Console.WriteLine("The variable from 't3' has changed:");
+            Console.WriteLine("The sending object was: {0}", sender);
+            Console.WriteLine(e.ToString());
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// For changes on t2.
+        /// </summary>
+        /// <param name="sender">Sending object.</param>
+        /// <param name="e">Event Arguments.</param>
+        private void T2_OnChange(object sender, ChangeEventArgs<object> e)
+        {
+            Console.WriteLine("The variable from 't2' has changed:");
+            Console.WriteLine("The sending object was: {0}", sender);
+            Console.WriteLine(e.ToString());
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// For changes on t1.
+        /// </summary>
+        /// <param name="sender">Sending object.</param>
+        /// <param name="e">Event Arguments.</param>
+        private void T1_OnChange(object sender, ChangeEventArgs<string> e)
+        {
+            Console.WriteLine("The variable from 't1' has changed:");
+            Console.WriteLine("The sending object was: {0}", sender);
+            Console.WriteLine(e.ToString());
+            Console.WriteLine();
+        }
+    }
+}
