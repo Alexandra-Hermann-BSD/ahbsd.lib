1b93fc463f835bfad7738bd082033fb2a0d2a08b
Heinrich Alexandra Hermann <a.hermann@hermann-bsd.de>
2021-04-03 16:15:20 +0200

    Added tests for new namespace NamedCollections

--- Test_xUnit/UnitTest1.cs
+++ Test_xUnit/UnitTest1.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using ahbsd.lib.Tools;
 using ahbsd.lib.Password;
 using ahbsd.lib.Password.Check;
+using ahbsd.lib.NamedCollections;
+using System.Linq;
 
 namespace Test_xUnit
 {
@@ -152,6 +154,106 @@ namespace Test_xUnit
             resultUSn = Checksum.GetFinalChecksum(tmpULn1);
             resultUSn = Checksum.GetFinalChecksum(tmpULn2);
             resultUSn = Checksum.GetFinalChecksum(tmpULn3);
+        }
+
+        [Fact]
+        public void TestNamedCollection()
+        {
+            INamedList<int> l1 = new NamedList<int>("l1", 11);
+            INamedCollection<int> nc1 = new NamedCollection<int>("NamedCollection 1 ", l1);
+            nc1.OnNameChanged += Nc1_OnNameChanged;
+            l1.OnNameChanged += Nc1_OnNameChanged;
+            
+
+            Console.WriteLine(nc1);
+            Console.WriteLine(l1);
+
+            l1.Name = "Named List #1";
+
+            for (int i = 0; i < 11; i++)
+            {
+                nc1.Add(i);
+            }
+
+            nc1.Name = "NamedCollection 1";
+            nc1.Name = "Collection 1, which is named.";
+
+            Console.WriteLine(nc1);
+            Console.WriteLine(l1);
+        }
+
+        [Fact]
+        public void TestDictionaryOfNamedCollections()
+        {
+            IDictionaryOfNamedCollections<int, double> d1
+                = new DictionaryOfNamedCollection<int, double>();
+            IDictionaryOfNamedCollections<string, string> d2
+                = new DictionaryOfNamedCollection<string, string>();
+
+            d1.OnNamedCollectionAdded += D1_OnNamedCollectionAdded;
+            d2.OnNamedCollectionAdded += D2_OnNamedCollectionAdded;
+
+            d1.Add(1, 1.0, "Double");
+            d1.Add(1, 1.1);
+
+            d2.Add("Apple", "iPhone X", "Apple Devices");
+            d2.Add("Apple", "iPad");
+        }
+
+        private void D2_OnNamedCollectionAdded(object sender, EventArgs<INamedCollection<string>> e)
+        {
+            Console.WriteLine("To {0} a new INamedCollection<string> was added:", sender);
+            Console.WriteLine(e.Value);
+        }
+
+        private void D1_OnNamedCollectionAdded(object sender, EventArgs<INamedCollection<double>> e)
+        {
+            Console.WriteLine("To {0} a new INamedCollection<double> was added:", sender);
+            Console.WriteLine(e.Value);
+        }
+
+        [Fact]
+        public void TestDictionaryOfNamedLists()
+        {
+            IDictionaryOfNamedList<int, double> l1
+                = new DictionaryOfNamedList<int, double>();
+            IDictionaryOfNamedList<string, string> l2
+                = new DictionaryOfNamedList<string, string>();
+
+            l1.OnNamedListAdded += L1_OnNamedListAdded;
+            l2.OnNamedListAdded += L2_OnNamedListAdded;
+
+            l1.Add(1, "Double");
+            l1.Add(1, 1.1);
+            l1.Add(1, 1.2);
+            l1.Add(1, 1.3);
+            l1.Add(2, 1.1, "Double 2");
+            l1.Add(2, 1.2);
+            l1.Add(2, 1.3);
+
+            l2.Add("Apple", "iPhone X", "Apple Devices");
+            l2.Add("Apple", "iPad");
+            l2.Add("Amazon", "Echo", "Amazon Devices");
+        }
+
+        private void L1_OnNamedListAdded(object sender, EventArgs<INamedList<double>> e)
+        {
+            Console.WriteLine("To {0} a new INamedList<double> was added:", sender);
+            Console.WriteLine(e.Value);
+        }
+
+        private void L2_OnNamedListAdded(object sender, EventArgs<INamedList<string>> e)
+        {
+            Console.WriteLine("To {0} a new INamedList<string> was added:", sender);
+            Console.WriteLine(e.Value);
+        }
+
+        private void Nc1_OnNameChanged(object sender, ChangeEventArgs<string> e)
+        {
+            Console.WriteLine("The named collections name has changed");
+            Console.WriteLine("The sending object was {0}", sender);
+            Console.WriteLine(e.ToString());
+            Console.WriteLine();
         }
 
         private void T3_OnChange(object sender, ChangeEventArgs<object> e)
